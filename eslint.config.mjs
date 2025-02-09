dc83f285b6900cd86dba280348ad18c6728fac09
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-02-09 16:58:12 +0900

    Disable the vue/multi-word-component-names ESLint rule for layouts/*.vue

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,4 +12,5 @@ export default [
   ...tseslint.configs.recommended,
   ...pluginVue.configs["flat/essential"],
   {files: ["**/*.vue"], languageOptions: {parserOptions: {parser: tseslint.parser}}},
+  {files: ["**/layouts/*.vue"], rules: {"vue/multi-word-component-names": "off"}},
 ];

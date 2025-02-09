6cfd25af180bd360fa444dae5b9a840d0f2a399a
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-02-09 23:08:01 +0900

    Create scripts/build.sh

---
+++ scripts/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -eu
+
+rootDir="$(pwd)"
+rootDist="${rootDir}/dist"
+talksDir="${rootDir}/talks"
+
+if [ -d "$rootDist" ]; then
+  echo "Cleaning existing ${rootDist} folder..."
+  rm -rf "$rootDist"
+fi
+mkdir -p "$rootDist"
+
+if [ ! -d "$talksDir" ]; then
+  echo "Error: ${talksDir} directory not found." >&2
+  exit 1
+fi
+
+for pkgDir in "${talksDir}"/*; do
+  if [ -d "$pkgDir" ]; then
+    dirName="$(basename "$pkgDir")"
+
+    pushd "$pkgDir"
+    pnpm build --base ${dirName} --out ${rootDist}/${dirName}
+    popd
+  fi
+done

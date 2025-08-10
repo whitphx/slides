0beae46c9c285c266eb5c0f9067a319943bcd7be
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-08-10 11:07:55 +0900

    Set blur on Modal

--- decks/202507-python-serverless-web-apps/components/Modal.vue
+++ decks/202507-python-serverless-web-apps/components/Modal.vue
@@ -1,5 +1,5 @@
 <template>
-  <div border="~ sky/50 rounded-lg" from-sky:10 to-indigo:10 bg-gradient-to-br :w="w">
+  <div border="~ sky/50 rounded-lg" from-sky:10 to-indigo:10 bg-gradient-to-br backdrop-blur-md :w="w">
     <div flex gap-2 items-center bg-sky:10 px4 py2 rounded text-md>
       <span grow>
         <slot name="title">
